269efd75aed0ba27a9b35c96a1c0e4cc8574d4e7
pbhuniar <82756479+pbhuniar@users.noreply.github.com>
2021-04-18 15:45:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "~slick-carousel/slick/slick.css"; 
+// import "~slick-carousel/slick/slick-theme.css";
+import Routing from './RouterFolder/Routing';
+
+function App() {
+  return (
+    <div className="App">
+<Routing></Routing>
+    </div>
+  );
+}
+
+export default App;
